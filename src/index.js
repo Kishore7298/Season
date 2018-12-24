bd4c496493664f126751a6128518943083367854
kishore <4nm15is025@nmamit.in>
2018-12-24 14:35:10 +0530

    making use of state to store latitude

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render(){
+    constructor(props){
+        super(props);
+        this.state = {lat:null};
+
         window.navigator.geolocation.getCurrentPosition(
-            (position)=>{console.log(position)},
+            (position)=>{
+                this.setState({lat:position.coords.latitude})
+            },
             (err)=>{console.log(err)}
         )
+    }
+
+    render(){
+        
         return (
-            <div>Hello There!</div>
+            <div>Lattitude: {this.state.lat}</div>
         )
     }
 }
