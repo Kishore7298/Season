02217fa97e649326ed7f32dbada752c4d375d7b7
kishore <4nm15is025@nmamit.in>
2018-12-24 16:22:35 +0530

    error handling with state

--- src/index.js
+++ src/index.js
@@ -4,20 +4,26 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     constructor(props){
         super(props);
-        this.state = {lat:null};
+        this.state = {lat:null,err:''};
 
         window.navigator.geolocation.getCurrentPosition(
             (position)=>{
                 this.setState({lat:position.coords.latitude})
             },
-            (err)=>{console.log(err)}
+            (err)=>{
+                this.setState({err:err.message})
+            }
         )
     }
 
     render(){
         
         return (
-            <div>Lattitude: {this.state.lat}</div>
+            <div>
+                Lattitude: {this.state.lat}
+                <br></br>
+                Error: {this.state.err}
+            </div>
         )
     }
 }
