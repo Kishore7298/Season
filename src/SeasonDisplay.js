9430c5669c5a3427630d495527d211340e7e1e51
kishore <4nm15is025@nmamit.in>
2018-12-27 11:47:18 +0530

    Ternary expression in JSX

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -2,16 +2,19 @@ import React from 'react';
 
 function getSeason(lat,month){
     if(month>=2 && month<=9 ){
-        return lat>0?"Summer":"winter";
+        return lat>0?"Summer":"Winter";
     } else {
         return lat>0?"Winter":"Summer";
     }
 }
 
 const SeasonDisplay = (props)=>{
-    let season = getSeason(props.lat,new Date().getMonth())
+    let season = getSeason(props.lat,new Date().getMonth());
+    let text = season === "Winter"?"burr! It's chilly!":"Let's hit the beach!";
     return (
-        <div>{season}</div>
+        <div>
+            <h1>{text}</h1>
+        </div>
     )
 }
 
